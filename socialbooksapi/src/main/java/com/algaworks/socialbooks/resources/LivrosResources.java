ccda42cffc8b2e82f57421141e80837fe91c9171
Lucas Souza <lucas-machado@outlook.com>
2017-08-30 21:45:44 -0300

    Inicio do versionamento do projeto que será desenvolvido durante a realização do curso de Spring RESTful da Algaworks;

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,24 @@
+package com.algaworks.socialbooks.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivroRepository;
+
+@RestController
+public class LivrosResources {
+
+	@Autowired
+	LivroRepository livroRepository;
+	
+	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	public List<Livro> listar() {
+		
+		return livroRepository.findAll();
+	}
+}
